6b46ba58accb5f2f7a03f6ce22c406efbc7d335d
upanshu bawankar <619bawankarupanshu@gmail.com>
2023-12-07 13:57:19 +0530

    Control Flow

---
+++ src/main/java/com/bootcoding/control flow/decision statement/AgeClassifier.java
@@ -0,0 +1,24 @@
+public class AgeClassifier
+{
+   public static void main(String [] args)
+     {
+         int a = 500;
+
+          if(a<=12)
+          {
+             System.out.println("Child");
+          }
+          else if (a>12 && a<19)
+           {
+             System.out.println("Teen Age");
+           }
+          else if (a>=20 && a<=50)
+           {
+             System.out.println("Adult");
+           }
+          else
+           {
+             System.out.println("Senior Citizen");
+           }
+     }
+}
