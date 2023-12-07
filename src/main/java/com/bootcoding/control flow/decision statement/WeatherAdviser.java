b767b8dfda9520ddb6e1e83669ab0967fa4b8db2
upanshu bawankar <619bawankarupanshu@gmail.com>
2023-12-07 16:28:49 +0530

    Control Flow

--- src/main/java/com/bootcoding/control flow/decision statement/WeatherAdviser.java
+++ src/main/java/com/bootcoding/control flow/decision statement/WeatherAdviser.java
@@ -2,15 +2,23 @@ public class WeatherAdviser
 {
    public static void main(String [] args)
     {
-       float f = 49.3f;
+       float f = -2.0f;
 
        if(f>=25.0)
         {
            System.out.println("Weather is hot you should wear shorts and t-shirt ");
         }
-       else
+       else if(f<25 && f >=10)
         {
-           System.out.println("Weather is hot you should wear coat");
+           System.out.println("Weather is cold you should wear coat");
+        }
+       else if(f<10 && f >=1)
+        {
+           System.out.println("Weather is extremely you should wear coat");
+        }
+       else 
+        {
+           System.out.println("Weather is freezing. Don't go outside the home");
         }
     }
 }
