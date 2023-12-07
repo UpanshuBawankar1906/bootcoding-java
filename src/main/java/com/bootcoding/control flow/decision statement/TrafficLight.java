197c74f708539034625f6d28abd66bc2d0360f05
upanshu bawankar <619bawankarupanshu@gmail.com>
2023-12-07 16:09:45 +0530

    Control Flow

---
+++ src/main/java/com/bootcoding/control flow/decision statement/TrafficLight.java
@@ -0,0 +1,24 @@
+public class TrafficLight
+{
+   public static void main(String [] args)
+     {
+        
+        String color = "red";
+     
+
+        switch(color)
+          {
+              case "red":
+              System.out.println("Green");
+              break;
+
+              case "green":
+              System.out.println("Yellow");
+              break;
+
+              case "yellow":
+              System.out.println("Red");
+              break;
+         }
+     }
+}
