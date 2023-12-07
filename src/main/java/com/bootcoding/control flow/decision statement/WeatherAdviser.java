4ce435ca79e6df23278745fcc24ee797c3a54789
upanshu bawankar <619bawankarupanshu@gmail.com>
2023-12-07 15:33:18 +0530

    Control Flow

---
+++ src/main/java/com/bootcoding/control flow/decision statement/WeatherAdviser.java
@@ -0,0 +1,16 @@
+public class WeatherAdviser
+{
+   public static void main(String [] args)
+    {
+       float f = 49.3f;
+
+       if(f>=25.0)
+        {
+           System.out.println("Weather is hot you should wear shorts and t-shirt ");
+        }
+       else
+        {
+           System.out.println("Weather is hot you should wear coat");
+        }
+    }
+}
