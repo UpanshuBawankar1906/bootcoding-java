553c53a36f7d996ac28197fe08b22cf632458c26
upanshu bawankar <619bawankarupanshu@gmail.com>
2023-12-08 16:03:48 +0530

    Merge Sorted Array

---
+++ src/main/java/com/bootcoding/array/MergeArray.java
@@ -0,0 +1,28 @@
+public class MergeArray 
+{
+    public void merge(int[] nums1, int m, int[] nums2, int n) 
+    {
+        int i1 = m-1;
+        int i2 = n-1;
+        int i = m+n-1;
+
+        while (i2 >= 0)
+        {
+            if(i1>=0 && nums1[i1] > nums2[i2])
+            {
+                nums1[i] = nums1[i1];
+                i--;
+                i1--;
+            }
+            else
+            {
+                nums1[i] = nums2[i2];
+                i--;
+                i2--;
+            }
+        }
+       
+    }
+        
+    
+}
