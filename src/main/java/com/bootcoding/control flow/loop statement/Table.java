215f12fa53ac2bbd651c719814764afdb684ec38
upanshu bawankar <619bawankarupanshu@gmail.com>
2023-12-11 15:32:17 +0530

    Loop Statement

---
+++ src/main/java/com/bootcoding/control flow/loop statement/Table.java
@@ -0,0 +1,14 @@
+public class Table
+{
+   public static void main(String [] args)
+     {
+       int i;
+       int num = 7; 
+
+          for( i = 1; i<=10; i++) 
+            {
+                 System.out.println(num + " x "+ i +" = " + num*i );
+            }
+         
+     }
+}
