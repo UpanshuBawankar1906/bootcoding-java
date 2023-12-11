1e1ad78d279061601f7455abc1563b199c158873
upanshu bawankar <619bawankarupanshu@gmail.com>
2023-12-11 13:35:52 +0530

    Loop Statement

---
+++ src/main/java/com/bootcoding/control flow/loop statement/Loop.java
@@ -0,0 +1,14 @@
+public class Loop
+{
+ public static void main(String [] args)
+
+  {
+      byte i;
+      for ( i = 1; i<= 20; )
+         {
+
+       System.out.println(i + " Java");
+       i++;
+          }
+  }
+}
