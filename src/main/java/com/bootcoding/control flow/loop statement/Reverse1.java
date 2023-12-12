b571433ab8df4f6aa19ffb5290f146597d00cd99
upanshu bawankar <619bawankarupanshu@gmail.com>
2023-12-12 14:53:33 +0530

    While Loop

---
+++ src/main/java/com/bootcoding/control flow/loop statement/Reverse1.java
@@ -0,0 +1,14 @@
+public class Reverse
+{  
+    public static void main(String[] args) 
+     {  
+        
+        
+
+        for (; ; ;) 
+             
+          {  
+            System.out.print(arr[i] + " ");  
+          } 
+}
+} 
