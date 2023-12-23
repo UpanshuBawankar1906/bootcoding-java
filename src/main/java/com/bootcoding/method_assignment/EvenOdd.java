5ea2c985853bb8a068b72eb73abd08007da1ff13
upanshu bawankar <619bawankarupanshu@gmail.com>
2023-12-23 21:25:00 +0530

    Finding EvenOdd...

---
+++ src/main/java/com/bootcoding/method_assignment/EvenOdd.java
@@ -0,0 +1,21 @@
+package com.bootcoding.method_assignment;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void checkEvenOdd(int number) {
+        if (number % 2 == 0) {
+            System.out.println(number + " is an even number.");
+        } else {
+            System.out.println(number + " is an odd number.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        checkEvenOdd(num);
+
+    }
+}
