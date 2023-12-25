54cddd214ab2daef20a16d9afe93b9fd41859e9e
upanshu bawankar <619bawankarupanshu@gmail.com>
2023-12-25 14:03:56 +0530

    Math Method

---
+++ src/main/java/com/bootcoding/math_method/Absolute.java
@@ -0,0 +1,9 @@
+package com.bootcoding.math_method;
+
+public class Absolute
+{
+    public static void main(String[] args) {
+        int a = Math.abs(-30);
+        System.out.println(a);
+    }
+}
---
+++ src/main/java/com/bootcoding/math_method/Ceiling.java
@@ -0,0 +1,9 @@
+package com.bootcoding.math_method;
+
+public class Ceiling
+{
+    public static void main(String[] args) {
+        double a = Math.ceil(8.9);
+        System.out.println(a);
+    }
+}
