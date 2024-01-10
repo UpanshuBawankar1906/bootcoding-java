873393d4e71abbff4e70c41855c605650fc41e6a
upanshu bawankar <619bawankarupanshu@gmail.com>
2024-01-10 14:25:33 +0530

    ArrayList

---
+++ src/main/java/com/bootcoding/arraylist/Example1.java
@@ -0,0 +1,27 @@
+package com.bootcoding.arraylist;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Example1 {
+    public static void main(String[] args) {
+
+
+        ArrayList al = new ArrayList();
+        al.add(2);
+        al.add(3.4);
+        al.add("Upanshu");
+        al.add("Fruit");
+        al.add("Bawankar");
+
+        System.out.println(al);
+        al.set(3,"sanjay");
+        al.remove(2);
+        Iterator itr=al.iterator();
+        while(itr.hasNext()){
+            System.out.println(itr.next());
+        }
+
+
+    }
+}
